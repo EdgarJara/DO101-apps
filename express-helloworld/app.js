a143240b531cfc773e99c966b14053f0a9543682
EdgarJara <edgarjarasalamea@gmail.com>
2021-06-29 16:44:11 -0500

    Change Add Sammael

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,13 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-<<<<<<< HEAD
 app.get('/sammael', function(req, res) {
   res.send('Hello Sammael...!\n');
-=======
-app.get('/mars', function(req, res) {
-  res.send('Hello Mars!\n');
->>>>>>> 3b07203a6679616422b7188e846f811f6376652b
   });
 
 app.listen(8080, function () {
